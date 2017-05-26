822804962d57c165d38a9f666e4112c64d2926a4
Mocanu Stefan-Alexandru <mocanu.stefan.alx@gmail.com>
2017-05-27 00:21:05 +0300

    First Day
    Api and SPA beginings

---
+++ LicentaAll/LicentaHighLevelApi/AppStart/AuthZeroConfig.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Owin.Security.DataHandler.Encoder;
+using Microsoft.Owin.Security.Jwt;
+using Owin;
+using System.Configuration;
+
+namespace LicentaHighLevelApi
+{
+    public partial class Startup
+    {
+        private void ConfigureAuthZero(IAppBuilder app)
+        {
+            var issuer = "https://" + ConfigurationManager.AppSettings["auth0:Domain"] + "/";
+            var audience = ConfigurationManager.AppSettings["auth0:ClientId"];
+            var secret = TextEncodings.Base64.Encode(TextEncodings.Base64Url.Decode(ConfigurationManager.AppSettings["auth0:ClientSecret"]));
+
+            app.UseJwtBearerAuthentication(new JwtBearerAuthenticationOptions
+            {
+                AuthenticationMode = Microsoft.Owin.Security.AuthenticationMode.Active,
+                AllowedAudiences = new[] { audience },
+                IssuerSecurityTokenProviders = new[] { new SymmetricKeyIssuerSecurityTokenProvider(issuer, secret) }
+            });
+        }
+    }
+}
---
+++ LicentaAll/LicentaHighLevelApi/Startup.cs
@@ -0,0 +1,25 @@
+﻿using LicentaHighLevelApi;
+using LicentaHighLevelApi.AppStart;
+using Microsoft.Owin;
+using Microsoft.Owin.Cors;
+using Owin;
+using System.Web.Http;
+
+[assembly: OwinStartup(typeof(Startup))]
+namespace LicentaHighLevelApi
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+
+            WebApiConfig.Register(config);
+
+            ConfigureAuthZero(app);
+
+            app.UseCors(CorsOptions.AllowAll);
+            app.UseWebApi(config);
+        }
+    }
+}
